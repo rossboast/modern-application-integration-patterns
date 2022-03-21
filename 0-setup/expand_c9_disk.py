fb9231820040cfbab83522b51fe548194f69c8b2
rmbo <33289376+rossboast@users.noreply.github.com>
2022-03-21 15:45:34 +0000

    Remove logging message

--- 0-setup/expand_c9_disk.py
+++ 0-setup/expand_c9_disk.py
@@ -9,7 +9,6 @@ volume_info = ec2.describe_volumes(
 volume_id = volume_info["Volumes"][0]["VolumeId"]
 try:
     resize = ec2.modify_volume(VolumeId=volume_id, Size=100)
-    print(resize)
 except ClientError as e:
     if e.response["Error"]["Code"] == "InvalidParameterValue":
         print("ERROR MESSAGE: {}".format(e))
